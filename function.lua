463eb4efc0170efbe2974f52e688805f4bf8cc8f
devvie <74534592+9551-Dev@users.noreply.github.com>
2026-03-26 13:50:53 +0100

    oh my god.

--- function.lua
+++ function.lua
@@ -1,33 +1,60 @@
-return setmetatable({__argstack={n=0},__proxy={},__retstack={n=0,head=1},__help={
-    shift=function(t,n)
-        local new = {n=(n>=0) and t.n+n or math.max(0,t.n-n),head=t.head}
-        for k,v in pairs(t) do if type(k) == "number" then new[k+n] = v end end
-        return new
-    end
-}},{
-    __newindex = function(self,key,val) self.__proxy[key] = val end,
-    __index = function(self,key)
-        local func = rawget(self.__proxy,key)
-        if func then
-            local ret = table.pack(pcall(func,table.unpack(
-                self.__argstack,1,self.__argstack.n
-            )))
-            if ret[1] then
-                self.__argstack = {n=0}
-                self.__retstack = table.pack(table.unpack(ret,2,ret.n))
-                self.__retstack.head = 1
-                return ret[2]
+local function bind_to(t,stack)
+    return setmetatable({
+        __proxy=t or {},
+        __argstack=stack and stack.__argstack or {ref={n=0}},
+        __retstack=stack and stack.__retstack or {ref={n=0,head=1}},
+        __help={
+            shift=function(t,n)
+                local new = {n=(n>=0) and t.n+n or math.max(0,t.n-n),head=t.head}
+                for k,v in pairs(t) do if type(k) == "number" then new[k+n] = v end end
+                return new
             end
-            error(ret[2],2)
-        end
-    end,
-    __call = function(self,key) return rawget(self.__proxy ,key) end,
-    __add  = function(self,val) self.__argstack[self.__argstack.n+1] = val self.__argstack.n = self.__argstack.n + 1 return self end,
-    __sub  = function(self,idx) table.remove(self.__argstack,idx)          self.__argstack.n = self.__argstack.n - 1 return self end,
-    __div  = function(self,idx) self.__argstack[idx] = nil return self end,
-    __mod  = function(self,idx) return self.__retstack[idx] end,
-    __unm  = function(self) self.__retstack.head = self.__retstack.head + 1 return self.__retstack[self.__retstack.head] end,
-    __lt   = function(self,cnt) self.__argstack = self.__help.shift(self.__argstack,-cnt) return self end,
-    __le   = function(self,cnt) self.__retstack = self.__help.shift(self.__retstack,-cnt) return self end,
-    __len  = function() return  self.__retstack.n end
-})
+        }
+    },{
+        __ispatched = true,
+        __newindex  = function(self,key,val) self.__proxy[key] = val end,
+        __concat    = function(self,str)
+            if str == "__unwrap" then return function(start,fin)
+                return table.unpack(self.__retstack.ref,start or self.__retstack.ref.head,fin or self.__retstack.ref.n)
+            end end
+        end,
+        __index = function(self,key)
+            local target = self.__proxy[key]
+            local tar_mt = getmetatable(target)
+            if type(target) == "function" then
+                local ret = table.pack(pcall(target,table.unpack(
+                    self.__argstack.ref,1,self.__argstack.ref.n
+                )))
+                if ret[1] then
+                    self.__argstack.ref = {n=0}
+                    self.__retstack.ref = table.pack(table.unpack(ret,2,ret.n))
+                    self.__retstack.ref.head = 1
+                    return ret[2]
+                end
+                error(ret[2],2)
+            elseif type(target) == "table" and not (tar_mt and tar_mt.__ispatched) then
+                return bind_to(target,self)
+            end
+        end,
+        __call = function(self,key)
+            local target = self.__proxy[key]
+            local tar_mt = getmetatable(target)
+            if type(target) == "table" and not (tar_mt and tar_mt.__ispatched) then
+                return bind_to(target,self)
+            end
+            return target
+        end,
+        __add = function(self,val)  self.__argstack.ref[self.__argstack.ref.n+1] = val self.__argstack.ref.n = self.__argstack.ref.n + 1 return self end,
+        __sub = function(self,idx)  table.remove(self.__argstack.ref,idx)    self.__argstack.ref.n = self.__argstack.ref.n - 1 return self end,
+        __div = function(self,idx)  self.__argstack.ref[idx] = nil return self end,
+        __mod = function(self,idx)  return self.__retstack.ref[idx] end,
+        __unm = function(self)      self.__retstack.ref.head = self.__retstack.ref.head + 1 return self.__retstack.ref[self.__retstack.ref.head] end,
+        __lt  = function(self,cnt)  self.__argstack.ref = self.__help.shift(self.__argstack.ref,-cnt) return self end,
+        __le  = function(self,cnt)  self.__retstack.ref = self.__help.shift(self.__retstack.ref,-cnt) return self end,
+        __len = function() return   self.__retstack.ref.n end,
+        __mul = function(self,head) self.__argstack.ref,self.__retstack.ref = {n=0},{n=0,head=head} return self end,
+        __pow = function(self,head) self.__retstack.ref.head = head end
+    })
+end
+
+return {bind_to=bind_to,src=bind_to,func=bind_to}
